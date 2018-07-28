14c03fd433f0e66bbe8878da36642cb5c6e2ad67
lambortao <lambortao@hotmail.com>
2018-07-28 23:08:12 +0800

    基本的目录结构

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '@/components/Home'
+let Home = () => import('@/components/Home');
 
 Vue.use(Router)
 
@@ -10,6 +10,11 @@ export default new Router({
       path: '/',
       name: 'Home',
       component: Home
+    },
+    {
+      path: '/',
+      name: 'Login',
+      component: Login
     }
   ]
 })
