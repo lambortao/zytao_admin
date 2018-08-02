5d82793d0b4fac88e4745aef2998b11f31affd31
lambortao <lambortao@hotmail.com>
2018-08-02 23:28:23 +0800

    路由建立好了

--- src/router/index.js
+++ src/router/index.js
@@ -37,6 +37,36 @@ export default new Router({
           path: 'article/edit',
           name: 'articleEdit',
           component: () => import('@/views/article/edit')
+        },
+        {
+          path: 'project',
+          name: 'project',
+          component: () => import('@/views/project/list')
+        },
+        {
+          path: 'project/edit',
+          name: 'projectEdit',
+          component: () => import('@/views/project/edit')
+        },
+        {
+          path: 'think',
+          name: 'think',
+          component: () => import('@/views/think/list')
+        },
+        {
+          path: 'think/edit',
+          name: 'thinkEdit',
+          component: () => import('@/views/think/edit')
+        },
+        {
+          path: 'welcome',
+          name: '欢迎页',
+          component: () => import('@/views/single/welcome')
+        },
+        {
+          path: 'user',
+          name: '个人中心',
+          component: () => import('@/views/single/user')
         }
       ]
     },
