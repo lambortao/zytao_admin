fbcd2cdadb03fb8e6a855691171805f1364a1720
lambortao <lambortao@hotmail.com>
2018-08-07 18:08:56 +0800

    开始抽离table-list

--- src/main.js
+++ src/main.js
@@ -5,9 +5,14 @@ import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import App from './App'
+import tableList from './libs/my-table.vue'
+import axios from 'axios';
 
-Vue.use(ElementUI, { size: 'small', zIndex: 3000 })
+Vue.use(ElementUI, { size: 'small', zIndex: 3000 });
+Vue.component('table-list', tableList);
 
+Vue.prototype.axios = axios;
+Vue.prototype.urlHost = process.env.NODE_ENV === 'development' ? 'http://localhost/hope/admin.php/' : config.dev.urlHost;
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
--- src/views/think/list.vue
+++ src/views/think/list.vue
@@ -1,12 +1,37 @@
 <template>
   <div id="thinkList">
     这里是说说的列表
+    <table-list></table-list>
   </div>
 </template>
 
 <script>
 export default {
-  
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+    getArticleList () {
+      // 这里是否能把这个函数抽离出来成一个公共的函数
+      this.axios({
+        method: 'post',
+        url: `${this.urlHost}article/getArticleList`,
+      }).then((req)=> {
+        console.log(req);
+        /**
+         * 这里需要传的东西可能会有很多
+         * 1、需要定义顶部工具栏需要哪些工具，如基础的新增，筛选，还有批量删除等
+         * 2、还要传一个数组来确定列表需要显示什么东西
+         * 3、列表后的操作工具栏需要显示什么东西
+         */
+      });
+    }
+  },
+  created () {
+    this.getArticleList();
+  }
 }
 </script>
 <style lang="scss" scoped>
